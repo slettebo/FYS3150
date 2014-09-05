981f32579a55a3b93ccd96b44f168938a1c8b12c
slettebo <magnussl@student.matnat.uio.no>
2014-09-05 15:17:04 +0200

    Deleted exercise folder

--- exercise2_2/exercise2_2_plot.py
+++
@@ -1,9 +0,0 @@
-from numpy import *
-import matplotlib.pyplot as plt
-
-i,diff = loadtxt('difference.dat', unpack=True)
-plt.figure()
-plt.loglog(10**i,diff,'ro', 10**i,diff,'-b')
-plt.xlabel('i')
-plt.ylabel('difference')
-plt.show()
