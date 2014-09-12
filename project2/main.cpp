bc0093ae2385e66e7bc61b456c4b6ba6f5c8d34d
slettebo <magnussl@student.matnat.uio.no>
2014-09-12 15:52:56 +0200

    Working jacobiRotation and maxelement functions. Not sure about the matrix generation of A... Gruppetime/datalab 12.sept.

--- project2/main.cpp
+++ project2/main.cpp
@@ -7,31 +7,84 @@ using namespace arma;
 using namespace std;
 
 
-// function for finding the largest element(s) A(j,k) in a SYMMETRIC matrix A:
-void largestElement(mat A, int N, int &k, int &l){
+// function for finding the largest off-diagonal element(s) A(j,k) in a SYMMETRIC matrix A:
+void largestElement(mat A, int N, int &k, int &l)
+{
     int i, j; // i = l, j = k
-    double max_value = A(0,1);  // calling first off-diagonal element equal to the largest element
+    double max_value = A(0,1);  // setting the 1st off-diag.el. = max. value element
     k = 0;
     l = 1;
 
-    for (i=0; i<N; i++){
-        for (j=i+1; j<N; j++){
-            if ( abs(max_value) - abs(A(i,j)) < 0){
+    for (i=0; i<N; i++)
+    {
+        for (j=i+1; j<N; j++)
+        {
+            if ( abs(A(i,j)) > abs(max_value))
+            {
                 max_value = A(i,j);
                 l = i;
                 k = j;
+                cout << "i" << i << endl;
+                cout << "j" << j << endl;
+                cout << "max_value" << max_value << endl;
             }
         }
     }
 }
 
+
+// "Jacobi rotation"-function:
+void jacobiRotation(mat &A, int k, int l, int N)
+{
+
+    int i;  // counter for the for-loop
+    double tau = ( A(l,l) - A(k,k) )/( 2*A(k,l) );  // constant for 2nd degree equation
+    double t_plus = - tau + sqrt(1 + tau*tau);      // 1st (+) root of tan(theta)
+    double t_minus = - tau - sqrt(1 + tau*tau);     // 2nd (-) root of tan(theta)
+    double t;                                       // declaring tan(theta)
+
+    // if-test to check and set tan(theta) equal to the smallest root:
+    if ( abs(t_plus) - abs(t_minus) < 0 )
+    {
+        t = t_plus;
+    }
+    else
+    {
+        t = t_minus;
+    }
+    cout << "t=" << t << endl;
+    double c = 1./(sqrt(1 + t*t));  // defining cos(theta) from tan(theta) using trig.-relation
+    double s = t*c;                 // defining sin(theta) from tan(theta) & cos(theta) using trig.-relation
+    cout << "c=" << c << "s=" << s << endl;
+    // for-loop with if-test to alter all diagonal elements (where i != l or k):
+    for (i=0; i<N; i++)
+    {
+        if (i!=k && i != l)
+        {
+            // B[i,i] = A[i,i];
+            A(i,k) = A(i,k)*c - A(i,l)*s;
+            A(k,i) = A(i,k);
+            A(i,l) = A(i,l)*c + A(i,k)*s;
+            A(l,i) = A(i,l);
+        }
+    }
+    // altering the non-diagonal elements of A
+    A(k,k) = A(k,k)*c*c - 2.0*A(k,l)*c*s + A(l,l)*s*s;
+    A(l,l) = A(l,l)*c*c + 2.0*A(k,l)*c*s + A(k,k)*s*s;
+    A(k,l) = 0;
+    A(l,k) = 0;
+}
+
+
+
 int main()
 {
     // declaration of constants and variables
     int i, j;
-    int N = 4;
+    int N = 10;
     int rho_min = 0;
-    double rho_max = 10;
+    double rho_max = 10e8;
+    double tolerance = 1e-10;
     double h = (rho_max - rho_min)/N;   // step size
     vec rho_array = linspace(rho_min,rho_max,N); // creating an array of equally spaced rhos
 
@@ -48,26 +101,39 @@ int main()
     A.diag(-1) = e;             // off-diagonal elements (below diag.) = e
 
 
-
+    /*
     // ARMADILLO-function for finding max.value- and index of max.value of a matrix
     uword row, col;
     double max_val = A.max(row,col);
     cout << "max value is at" << row << ',' << col << endl;
-
+    */
+    cout << "start \n" << A << endl;
     k = 0;
     l = 0;
-    largestElement(A,N,k,l);
+    largestElement(A,N,k,l);    // finding the first
+    cout << "k=" << k << ", l=" << l << endl;
 
-    cout << k << l << endl;
+    double maxvalue = abs(A(l,k));
 
+    cout << "abs(maxvalue)" << abs(A(l,k)) << endl;
 
-
-    /*
-    // "Jacobi rotation"-function:
-
-    void jacobiRotation(mat &A, int k, int l){
+    while (maxvalue > tolerance)
+    {
+        cout << "intheloop" << endl;
+        largestElement(A,N,k,l);
+        maxvalue = abs(A(l,k));
+        cout << "k=" << k << ", l=" << l << endl;
+        jacobiRotation(A,k,l,N);
+        cout << A << endl;
 
     }
+    cout << "end \n" << A << endl;
+
+    cout << "jacobi-method found eigvals:" << A.diag() << endl;
+    vec eigval = eig_sym(A);
+    cout << "armadillo found eigvals:" << eigval << endl;
+    /*
+
 
     /*
 
